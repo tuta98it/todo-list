dd524841c347195300c63488dbadd2bead49121a
Trần Anh Tú <47886966+TranAnhTu1998@users.noreply.github.com>
2023-02-19 17:45:58 +0700

    The To Do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,26 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
-  <h1>
-    Welcome to {{ title }}!
-  </h1>
-  <app-input-button-unit></app-input-button-unit>
-  <app-input-button-unit></app-input-button-unit>
-  <app-input-button-unit></app-input-button-unit>
-`,
+    <h1 *ngIf="">Welcome to {{ title }}!</h1>
+    <app-input-button-unit></app-input-button-unit>
+    <app-input-button-unit></app-input-button-unit>
+    <app-input-button-unit></app-input-button-unit>
+
+    <ul>
+      <li *ngFor="let todoItem of todoList">
+        {{ todoItem.title }}
+      </li>
+    </ul>
+  `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list';
+  todoList = [
+    { title: 'install NodeJS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+  ];
 }
