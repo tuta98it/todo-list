0523695430c7c59d5dedd3301881da7b509983ee
Trần Anh Tú <47886966+TranAnhTu1998@users.noreply.github.com>
2023-02-19 17:02:40 +0700

    Explore

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -5,13 +5,13 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <p>input-button-unit works! The title is: {{ title }}</p>
     <input
+      #inputElementRef
       type="text"
       [value]="title"
-      (keyup.enter)="changeTitle($event)"
-      id="my-input"
-    >
+      (keyup.enter)="changeTitle_ext(inputElementRef)"
+    />
 
-    <button (click)="changeTitle($event)">Save</button>
+    <button (click)="changeTitle_ext(inputElementRef)">Save</button>
     <!-- <button>Save</button> -->
   `,
   styleUrls: ['./input-button-unit.component.scss'],
@@ -38,6 +38,11 @@ export class InputButtonUnitComponent implements OnInit {
     const target = e.target as HTMLInputElement;
 
     this.title = target.value; // the original functionality still works
+  }
+
+  changeTitle_ext(inputElementReference: any) {
+    // console.log('inputElementReference = ' + inputElementReference)
+    this.title = inputElementReference.value; // the original functionality still works
   }
 
   generateTitle(): string {
